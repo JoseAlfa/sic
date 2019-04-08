2ba596b4d79b40c2b1c4be4e76753ae69c53d5b3
José Jiménez <josejimenezsanchez180697@gmail.com>
2019-04-08 10:00:05 -0500

    Create empresa.php

---
+++ presupuestos/application/views/ajax/empresa.php
@@ -0,0 +1,15 @@
+<div class="row">
+	<div class="col-xs-12 col-sm-6 col-md-4">
+		<div class="form-group form-float form-group-lg">
+            <div class="form-line">
+                <input type="text" class="form-control" />
+                <label class="form-label">Large Input</label>
+            </div>
+        </div>
+	</div>
+	<div class="col-xs-12 col-sm-6 col-md-4"></div>
+	<div class="col-xs-12 col-sm-6 col-md-4"></div>
+	<div class="col-xs-12 col-sm-6 col-md-4"></div>
+	<div class="col-xs-12 col-sm-6 col-md-4"></div>
+	<div class="col-xs-12 col-sm-6 col-md-4"></div>
+</div>
