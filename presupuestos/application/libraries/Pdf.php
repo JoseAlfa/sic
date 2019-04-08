b7264522fae70c8141f6394659d7451289047d55
José Jiménez <josejimenezsanchez180697@gmail.com>
2019-04-08 09:59:58 -0500

    Update Pdf.php

--- presupuestos/application/libraries/Pdf.php
+++ presupuestos/application/libraries/Pdf.php
@@ -2,51 +2,10 @@
  
 require_once dirname(__FILE__) . '/tcpdf/tcpdf.php';
  
-class Pdf extends TCPDF{
-
-	var $logo='./img/logos/logo.png';
-	var $font='helvetica';
-	var $fontsize='10';
-	var $datosEmpresa='Andador antonio Soler #114 Col Infonavit Atasta';
-
-
-	
-
-    /**
-	 * Initialize Preferences
-	 *
-	 * @access	public
-	 * @param	array	initialization parameters
-	 * @return	void
-	 */
-	public function initialize($params = array()){
-		if (is_array($params)) {
-			if (count($params) > 0)	{
-				foreach($params as $key => $val){
-					if (isset($this->$key))	{
-						$this->$key = $val;
-					}
-				}		
-			}
-		}
-		
-	}
-    public function Header() {
-        // Logo
-        $image_file = $this->logo;
-        //$this->Image($image_file, 17, 17,75, 40, 'PNG', '', 'T', false, 150, '', false, false, 0, false, false, false);
-        // Set font
-        $this->SetFont($this->font, 'B', $this->fontsize);
-        // Title
-        $this->Cell(0, 15, '', 0, false, 'C', 0, '', 0, false, 'M', 'M');
-    }
-    // Page footer
-    public function Footer() {
-        // Position at 15 mm from bottom
-        $this->SetY(-15);
-        // Set font
-        $this->SetFont('helvetica', 'I', 8);
-        // Page number
-        $this->Cell(0, 10, 'Página '.$this->getAliasNumPage().' de '.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, '', 'M');
+class Pdf extends TCPDF
+{
+    function __construct()
+    {
+        parent::__construct();
     }
 }
