4237aa3f9d967333e236b49bc9addb9efc19b77d
José Jiménez <josejimenezsanchez180697@gmail.com>
2019-04-08 09:59:14 -0500

    Update Load_view.php

--- presupuestos/application/controllers/Load_view.php
+++ presupuestos/application/controllers/Load_view.php
@@ -53,6 +53,23 @@ class Load_view extends CI_Controller {
             $this->load->view(getError('ajax'));//Vista que muestra errors
         }  
     }
+    private function hacerFecha($fecha=false,$larga=false){
+        $consulta=$this->modelo->getDate($fecha);
+        $fecha='';
+        $this->load->helper('fecha');
+        if ($consulta!=null) {
+            foreach ($consulta as $data) {
+                $dia=$data->d;
+                $dianombre=$data->dn;
+                $mes=$data->m;
+                $mesnombre=$data->mn;
+                $ano=$data->y;
+                $hora=$data->hr;
+                $fecha=getDateNom($dia,$mes,$ano,$dianombre,$larga);
+            }
+        }
+        return $fecha;
+    }
     /////////////paginación//////////////////
     function pagConfig($q="",$limit=10,$vista='#contenedor_general'){///Funcion que retorna la configuracion basica de la paginacion
         $config['div'] = $vista;//asignamos un id al contendor general            
@@ -114,8 +131,21 @@ class Load_view extends CI_Controller {
     function editScript(){//crea la llamada de la funcion que carga los detalles
         return '<script>$(\'.detail\').click(function(){$.sic.nuevo($(this).attr(\'ur\'),$(this).attr(\'ref\'));});</script>';
     }
-    function isAdmin($iduser,$admin=true){//Verific si es administrador el usuario
-        return true;
+    function isAdmin($iduser,$admin=false){//Verific si es administrador el usuario
+        $permiso=false;
+        $consulta=$this->modelo->userdataSes($iduser);
+        if ($consulta!=null) {
+            foreach ($consulta as $valor) {
+                if ($admin) {
+                    if ($valor->adm==1 || $valor->adm=='1') {
+                        $permiso=true;
+                    }
+                }else{
+                    $permiso=true;
+                }
+            }
+        }
+        return $permiso;
     }
     /*************************************************************************/
     /**Fin deFunciones generales para uso general de el resto de los modulos**/
@@ -174,7 +204,7 @@ class Load_view extends CI_Controller {
         $consulta=$this->modelo->getProductos($id);
         if ($consulta!=null) {
             foreach ($consulta as $val) {
-                $retornar=array('id'=>$val->id,'marcas'=>$this->marcasSelect($val->idmarca),'tipoPro'=>$this->tiposProSelect($val->idtipo),'nom'=>$val->nom,'cve'=>$val->cv,'det'=>$val->det,'img'=>$val->im);
+                $retornar=array('id'=>$val->id,'pre'=>$val->prec,'marcas'=>$this->marcasSelect($val->idmarca),'tipoPro'=>$this->tiposProSelect($val->idtipo),'nom'=>$val->nom,'cve'=>$val->cv,'det'=>$val->det,'img'=>$val->im,'med'=>$val->med);
             }
         }
         return $retornar;
@@ -426,7 +456,7 @@ class Load_view extends CI_Controller {
         $consulta=$this->modelo->getClientes($id);
         if ($consulta!=null) {
             foreach ($consulta as $val) {
-                $retornar=array('id'=>$val->id,'nom'=>$val->nom,'cor'=>$val->cor,'tel'=>$val->tel,'dir'=>$val->dir,'em'=>$val->em,'show'=>$val->show);
+                $retornar=array('id'=>$val->id,'nom'=>$val->nom,'cor'=>$val->cor,'tel'=>$val->tel,'dir'=>$val->dir,'em'=>$val->em,'show'=>$val->show,'cp'=>$val->cp,'atn'=>$val->atn);
             }
         }
         return $retornar;
@@ -434,45 +464,148 @@ class Load_view extends CI_Controller {
     /*************************************************************************/
     /*********Funciones de reportes***********/
 
+    private function clientesSel($id=false) {
+        $query=$this->modelo->getClientes();
+        $sel='<option value="undefined">Seleccione una opción</option>';
+        if ($query!=null) {
+            foreach ($query as $val) {
+                if ($val->id==$id) {
+                    $sel.='<option selected value="'.$val->id.'">'.$val->nom.'</option>';
+                }else{
+                    $sel.='<option value="'.$val->id.'">'.$val->nom.'</option>';
+                }
+            }
+        }
+        return $sel;
+    }
+    private function productosSel($id=false) {
+        $query=$this->modelo->getProductos();
+        $sel='<option value="undefined">Seleccione una opción</option>';
+        if ($query!=null) {
+            foreach ($query as $val) {
+                if ($val->id==$id) {
+                    $sel.='<option selected value="'.$val->id.'">'.$val->nom.'</option>';
+                }else{
+                    $sel.='<option id="productoSel'.$val->id.'" value="'.$val->id.'">'.$val->nom.'</option>';
+                }
+            }
+        }
+        return $sel;
+    }
+    private function productsJSON() {
+        $query=$this->modelo->getProductos();
+        $arr="";
+        if ($query!=null) {
+            $arr=json_encode($query);
+        }
+        return $arr;
+    }
+    public function newPresupuesto(){
+        if ($this->input->is_ajax_request()) {
+            $idu=$this->session->userdata('iduser');
+            $idp=$this->session->userdata('idperson');
+            $print="";
+            if ($idu) {
+                if ($this->isAdmin($idu)) {
+                    $data['clientes']=$this->clientesSel();
+                    $data['productos']=$this->productosSel();
+                    $data['json']=$this->productsJSON();
+                    $data['nuevo']=true;
+                    $this->load->view('ajax/presupuestos',$data);
+                }else{
+                    $print=getError('acceso');
+                }
+            }else{
+                $print=getError('sesion');
+            }
+            echo $print;
+        }else{
+            $this->load->view(getError('ajax'));//Vista que muestra errors
+        }
+    }
+
+    private function presupuestosTable($consulta,$busqueda,$padre,$detalles) {
+        $print=$this->busquedaForm($padre,$busqueda,$detalles);///formulario de busqueda
+        $encabezado=array('Nombre','Correo','Telefono','Usuario','Tipo');//Encabezado de la tablaa
+        $print.=$this->hacerEncabezado($encabezado);///Se le anexa el encabeado a la tabla
+        if ($consulta!=null&&$consulta) {//Si hay resulados en la consulta
+            foreach ($consulta as $val) {//Se recorre el resltado de la consulta
+                $id=$val->idp;///Id de persona
+                $idu=$val->idu;//id de usuario
+                $nombre=$val->nom;//nombre
+                $apellidos=$val->ap;//apellidos
+                $correo=$val->cor;//correo
+                $telefono=$val->tel;//correo
+                $admin=$val->adm;///Tipo de usuario
+                $usr=$val->usr;//ombre de usuario
+                $tipo="Administrador";//tipo de usuario en nombre
+                if (!$admin) {//Si no es administrador
+                    $tipo="Usuario normal";//tipo de usuario en nombre
+                }
+                $print.='<tr class="detail" ur="user" ref="'.$id.'"><td>'.$nombre.' '.$apellidos.'</td><td>'.$correo.'</td><td>'.$telefono.'</td><td>'.$usr.'</td><td>'.$tipo.'</td></tr>';
+            }
+        }else{
+            $print.='<tr><td colspan="'.count($encabezado).'">No hay resultados</td></tr>';//No hay resultado
+        }
+        return $print.='</tbody></table></div>';
+    }
+
+    public function presupuesto(){
+         if ($this->input->is_ajax_request()) {
+            $idu=$this->session->userdata('iduser');
+            $idp=$this->session->userdata('idperson');
+            $print="";
+            if ($idu) {
+                $consulta=null;
+                if ($this->isAdmin($idu)) {
+                    $ref=$this->input->post('ref',true);
+                    $consulta=$this->modelo->recientesPre(false,1,$ref);
+                    if ($consulta!=null) {
+                        $data=array();
+                        foreach ($consulta as $val) {
+                            $data['detalle']=$val->det;
+                            $data['pago']=$val->pago;
+                            $data['iva']=$val->iva;
+                            $data['idpre']=$val->id;
+                            #$data['']=$val->;
+                            $data['vencimiento']=$val->ven;
+                            $data['clientes']=$this->clientesSel($val->idc);
+                            $data['productos']=$this->productosSel();
+                            $data['json']=$this->productsJSON();
+                            $this->load->view('ajax/presupuestos',$data);
+                        }
+                    }
+                }else{
+                    $print=getError('acceso');
+                }
+            }else{
+                $print=getError('sesion');
+            }
+            echo $print;
+        }else{
+            $this->load->view(getError('ajax'));//Vista que muestra errors
+        }
+    }
     public function getPresupuestos($offset=0){
         if ($this->input->is_ajax_request()) {
             $idu=$this->session->userdata('iduser');
             $idp=$this->session->userdata('idperson');
             if ($idu) {
+                $print='<div class="row"><div class="col-sm-1"></div><div class="col-sm-10"><br><br><div class="list-group">';
+                $consulta=null;
                 if ($this->isAdmin($idu,true)) {
-                    $q=$this->input->post('q',true);///busqueda
-                    $config=$this->pagConfig($q,5);//se carga configuración predeterminada
-                    $config['base_url'] = './Load_view/usuarios';//url donde estan los datos a paginar
-                    $limit=$config['per_page'];///limete de la consulta
-                    $config['total_rows'] = $this->modelo->getUsuarios(false,$limit,$offset,false,$q,$idp);//total de registros
-                    $this->jquery_pagination->initialize($config);//se inicializa la librebreria de paginación
-                    $consulta =$this->modelo->getUsuarios(false,$limit,$offset,true,$q,$idp);//consulta de valores pertinentes a esta página
-                    $print=$this->busquedaForm('usuarios',$q,'user');///formulario de busqueda
-                    $encabezado=array('Nombre','Correo','Telefono','Usuario','Tipo');//Encabezado de la tablaa
-                    $print.=$this->hacerEncabezado($encabezado);///Se le anexa el encabeado a la tabla
-                    if ($consulta!=null&&$consulta) {//Si hay resulados en la consulta
-                        foreach ($consulta as $val) {//Se recorre el resltado de la consulta
-                            $id=$val->idp;///Id de persona
-                            $idu=$val->idu;//id de usuario
-                            $nombre=$val->nom;//nombre
-                            $apellidos=$val->ap;//apellidos
-                            $correo=$val->cor;//correo
-                            $telefono=$val->tel;//correo
-                            $admin=$val->adm;///Tipo de usuario
-                            $usr=$val->usr;//ombre de usuario
-                            $tipo="Administrador";//tipo de usuario en nombre
-                            if (!$admin) {//Si no es administrador
-                                $tipo="Usuario normal";//tipo de usuario en nombre
-                            }
-                            $print.='<tr class="detail" ur="user" ref="'.$id.'"><td>'.$nombre.' '.$apellidos.'</td><td>'.$correo.'</td><td>'.$telefono.'</td><td>'.$usr.'</td><td>'.$tipo.'</td></tr>';
-                        }
-                    }else{
-                        $print.='<tr><td colspan="'.count($encabezado).'">No hay resultados</td></tr>';//No hay resultado
-                    }
-                    $print.='</tbody></table></div><div class="paginas">'.$this->jquery_pagination->create_links().'</div>'.$this->editScript();
+                    $consulta=$this->modelo->recientesPre();
                 }else{
-                    $print=getError('acceso');
+                    $consulta=$this->modelo->recientesPre($idu);
                 }
+                if ($consulta!=null) {
+                    foreach ($consulta as $val) {
+                        $print.='<button type="button" class="list-group-item recent" onclick="$.sic.load(\'presupuesto\',\''.$val->cv.'\',{ref:'.$val->id.'})">'.$val->cv.' - '.$val->det.'<span class="badge bg-blue">Por '.$val->nom.' '.$val->ap.'</span></button>';
+                    }
+                }else{
+                    $print.='<button type="button" class="list-group-item">No hay resultados</button>';
+                }
+                $print.='</div></div></div>';
             }else{
                 $print=getError('sesion');
             }
@@ -533,37 +666,44 @@ class Load_view extends CI_Controller {
             $idu=$this->session->userdata('iduser');
             $idp=$this->session->userdata('idperson');
             if ($idu) {
-                if ($this->isAdmin($idu,true)) {
+                if ($this->isAdmin($idu,false)) {
                     $q=$this->input->post('q',true);///busqueda
                     $config=$this->pagConfig($q,5);//se carga configuración predeterminada
                     $config['base_url'] = './Load_view/usuarios';//url donde estan los datos a paginar
                     $limit=$config['per_page'];///limete de la consulta
-                    $config['total_rows'] = $this->modelo->getUsuarios(false,$limit,$offset,false,$q,$idp);//total de registros
+                    $config['total_rows'] = $this->modelo->presupuestos(false,1,$limit,$offset,false,$q,$idp);//total de registros
                     $this->jquery_pagination->initialize($config);//se inicializa la librebreria de paginación
-                    $consulta =$this->modelo->getUsuarios(false,$limit,$offset,true,$q,$idp);//consulta de valores pertinentes a esta página
+                    $consulta =$this->modelo->presupuestos(false,1,$limit,$offset,true,$q,$idp);//consulta de valores pertinentes a esta página
                     $print=$this->busquedaForm('usuarios',$q,'user');///formulario de busqueda
-                    $encabezado=array('Nombre','Correo','Telefono','Usuario','Tipo');//Encabezado de la tablaa
+                    $canSeeAut=$this->isAdmin($idu,true);
+                    $encabezado=array('Clave','Detalles','Cliente','Cerrado');//Encabezado de la tablaa
+                    if($canSeeAut){
+                        $encabezado=array('Clave','Detalles','Cliente','Cerrado','Creado por');//Encabezado de la tablaa
+                    }
                     $print.=$this->hacerEncabezado($encabezado);///Se le anexa el encabeado a la tabla
                     if ($consulta!=null&&$consulta) {//Si hay resulados en la consulta
                         foreach ($consulta as $val) {//Se recorre el resltado de la consulta
-                            $id=$val->idp;///Id de persona
-                            $idu=$val->idu;//id de usuario
-                            $nombre=$val->nom;//nombre
-                            $apellidos=$val->ap;//apellidos
-                            $correo=$val->cor;//correo
-                            $telefono=$val->tel;//correo
-                            $admin=$val->adm;///Tipo de usuario
-                            $usr=$val->usr;//ombre de usuario
-                            $tipo="Administrador";//tipo de usuario en nombre
-                            if (!$admin) {//Si no es administrador
-                                $tipo="Usuario normal";//tipo de usuario en nombre
+                            $id=$val->id;///Id pre
+                            $clave=$val->cv;//clave de pre
+                            $det=$val->det;///detalles de presupuesto
+                            $cliente=$val->cliente;
+                            $cerrado=$val->fin;//fin
+                            $creador=$val->nom.' '.$val->ap;
+                            $ini=$this->hacerFecha($val->ini);
+                            $fin=$this->hacerFecha($val->fin);
+                            $ref='code,'.$idu.','.$id;
+                            $ref=base64_encode($ref);
+                            $datos=json_encode($val);
+                            $print.='<tr class="detailjava" data-ref="'.$ref.'" data-detalle=\''.$datos.'\' data-see="'.$canSeeAut.'" data-inicio="'.$ini.'" data-fin="'.$fin.'"><td>'.$clave.'</td><td>'.$det.'</td><td>'.$cliente.'</td><td>'.$fin.'</td>';
+                            if($canSeeAut){
+                                $print.='<td>'.$creador.'</td>';
                             }
-                            $print.='<tr class="detail" ur="user" ref="'.$id.'"><td>'.$nombre.' '.$apellidos.'</td><td>'.$correo.'</td><td>'.$telefono.'</td><td>'.$usr.'</td><td>'.$tipo.'</td></tr>';
+                            $print.='</tr>';
                         }
                     }else{
                         $print.='<tr><td colspan="'.count($encabezado).'">No hay resultados</td></tr>';//No hay resultado
                     }
-                    $print.='</tbody></table></div><div class="paginas">'.$this->jquery_pagination->create_links().'</div>'.$this->editScript();
+                    $print.='</tbody></table></div><div class="paginas">'.$this->jquery_pagination->create_links().'</div> <script>$(".detailjava").click(function(e){$.sic.loadJS($(this));});</script>';
                 }else{
                     $print=getError('acceso');
                 }
@@ -605,12 +745,34 @@ class Load_view extends CI_Controller {
                             if (!$admin) {//Si no es administrador
                                 $tipo="Usuario normal";//tipo de usuario en nombre
                             }
-                            $print.='<tr class="detail" ur="user" ref="'.$id.'"><td>'.$nombre.' '.$apellidos.'</td><td>'.$correo.'</td><td>'.$telefono.'</td><td>'.$usr.'</td><td>'.$tipo.'</td></tr>';
+                            $ref='code,'.$idu.','.$id;
+                            $ref=base64_encode($ref);
+                            $print.='<tr class="detail" ur="user" data-ref="'.$ref.'"><td>'.$nombre.' '.$apellidos.'</td><td>'.$correo.'</td><td>'.$telefono.'</td><td>'.$usr.'</td><td>'.$tipo.'</td></tr>';
                         }
                     }else{
                         $print.='<tr><td colspan="'.count($encabezado).'">No hay resultados</td></tr>';//No hay resultado
                     }
                     $print.='</tbody></table></div><div class="paginas">'.$this->jquery_pagination->create_links().'</div>'.$this->editScript();
+                }else{
+                    $print=getError('acceso');
+                }
+            }else{
+                $print=getError('sesion');
+            }
+            echo $print;
+        }else{
+            $this->load->view(getError('ajax'));//Vista que muestra errors
+        }
+    }
+    ///////////////////detalles de empresa ////////////////////////////////////
+    public function dataPDF(){
+        if ($this->input->is_ajax_request()) {
+            $print="";
+            $idu=$this->session->userdata('iduser');
+            $idp=$this->session->userdata('idperson');
+            if ($idu) {
+                if ($this->isAdmin($idu,true)) {
+                    $this->load->view('ajax/empresa');
                 }else{
                     $print=getError('acceso');
                 }
